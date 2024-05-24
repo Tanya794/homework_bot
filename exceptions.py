f8e79d52576d0ceabc905674c24a1749482a2767
https://github.com/Tanya794 <tatyana711994@gmail.com>
2024-05-23 22:07:25 -0700

    Removed 'pass' in exception classes

--- exceptions.py
+++ exceptions.py
@@ -1,34 +1,22 @@
 class EndpointNotWork(Exception):
     """Недоступность endpoint."""
 
-    pass
-
 
 class NotExpectedAPIKeys(Exception):
     """Отсутствие ожидаемых ключей в ответе API."""
-
-    pass
 
 
 class UnexpectedHomeworkStatus(Exception):
     """Неожиданный статус домашней работы."""
 
-    pass
-
 
 class MessageNotSent(Exception):
     """Cбой при отправке сообщения в Telegram."""
-
-    pass
 
 
 class NoHomeworkName(Exception):
     """Нет названия домашней работы."""
 
-    pass
-
 
 class NotExpectedAPIStatus(Exception):
     """Ошибка статуса API ответа."""
-
-    pass
